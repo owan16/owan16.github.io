b2c4bed10ac01f2ba826a632fc5887ccbb37e2a3
owan16 <owanliu16@gmail.com>
2020-03-14 23:52:14 +0800

    Update utilities.js

--- Javascript/utilities.js
+++ Javascript/utilities.js
@@ -127,6 +127,10 @@ class Utilities {
 	}
 	
 	a01() {
+		var script = document.createElement('script');
+		script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
+		script.type = 'text/javascript';
+		document.getElementsByTagName('head')[0].appendChild(script);
 		$.ajax({
 			type: 'GET',
 			url: 'https://cors-anywhere.herokuapp.com/http://127.0.0.1:8080/services/pedometer/data/ax',
